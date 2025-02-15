d55c54508a20838a2b1382b187bbb251119b6b29
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-15 23:11:34 +0500

    Boolean21 added

---
+++ src/boolea/Boolean21.java
@@ -0,0 +1,24 @@
+package boolea;
+
+import java.util.Scanner;
+
+public class Boolean21 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Uch xonali son kiriting: ");
+        int son = scanner.nextInt();
+        if (son > 99 && son < 1000) {
+            int a = son % 10;       // birlik
+            int b = son / 10 % 10;  // o'nlik
+            int c = son / 100;      // yuzlik
+            if (c < b && b < a) {
+                System.out.println("Sonning raqamlari o'sish tartibida.");
+            } else {
+                System.out.println("Sonning raqamlari o'sish tartibida emas.");
+            }
+        } else {
+            System.out.println("Uch xonali son kiriting.");
+        }
+
+    }
+}
