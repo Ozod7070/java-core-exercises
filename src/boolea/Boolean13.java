0b470771c3d84848a545c879e30e354c25026457
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-15 22:48:25 +0500

    Boolean13 added

---
+++ src/boolea/Boolean13.java
@@ -0,0 +1,18 @@
+package boolea;
+
+import java.util.Scanner;
+
+public class Boolean13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Qiymat kiriting A: ");
+        int A = scanner.nextInt();
+        System.out.print("Qiymat kiriting B: ");
+        int B = scanner.nextInt();
+        System.out.print("Qiymat kiriting C: ");
+        int C = scanner.nextInt();
+        int tekshir = (A > 0 && B < 0 && C < 0) || (A < 0 && B > 0 && C < 0) || (A < 0 && B < 0 && C > 0) ? 1 : 0;
+        System.out.println((tekshir == 1));
+
+    }
+}
