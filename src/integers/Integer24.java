1ed1b85099cc19474c90a5ddd6ce85e613f1a45c
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-15 19:22:34 +0500

    Integer24 added

---
+++ src/integers/Integer24.java
@@ -0,0 +1,16 @@
+package integers;
+
+import java.util.Scanner;
+
+public class Integer24 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Son kirting 1-365 gacha: ");
+        int k = scanner.nextInt();
+        int kun = (k - 1) % 7;
+        String[] kunlar = {"Yakshanba", "Dushanba", "Seshanba", "Chorshanba", "Payshanba", "Juma", "Shanba"};
+        System.out.println("k soni "+k+ " shu hafta kuniga to'g'ri keladi "+kunlar[kun]);
+
+
+    }
+}
