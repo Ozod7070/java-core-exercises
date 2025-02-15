6d30c3c74f59844d28562aef220abedea468f9f6
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-15 18:32:31 +0500

    Integer02 added

--- src/integers/Integer02.java
+++ src/integers/Integer02.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class Integer02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Uzaunlikni kiriting sm: ");
-        int sm = scanner.nextInt();
-        int metr = sm / 100;
-        System.out.println("Toliq metr: "+metr);
+        System.out.print("Og'irlikni kiriting kg: ");
+        int kg = scanner.nextInt();
+        if (kg > 1000) {
+            int tonna = kg / 1000;
+            System.out.println("Toliq tonna: " + tonna);
+        }
     }
 }
