250c5577f71a69bf9ebd5aa5a1bdb00890bbd9b1
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-15 13:41:23 +0500

    First commit

---
+++ src/begins/Begin03.java
@@ -0,0 +1,13 @@
+package begins;
+
+import java.util.Scanner;
+
+public class Begin03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Kvadratning tomonini kiririing a: ");
+        double a = scanner.nextDouble();
+        double perimetr =  (4 * a);
+        System.out.println("Kvadratning perimetiri: "+ perimetr);
+    }
+}
