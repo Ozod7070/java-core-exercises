dcca16a38db15481f75c4378fe46be0868d1ceb0
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-15 19:13:40 +0500

    Integer20 added

---
+++ src/integers/Integer20.java
@@ -0,0 +1,14 @@
+package integers;
+
+import java.util.Scanner;
+
+public class Integer20 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Sekund kiriting: ");
+        int sekund = scanner.nextInt();
+        int soat = sekund / (60 * 60);
+        System.out.println("To'la o'tgan soat: "+soat);
+
+    }
+}
