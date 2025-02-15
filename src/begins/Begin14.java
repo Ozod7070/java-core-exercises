31ecc68b454494797b708ed0a4344ed799ae9fb9
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-15 15:12:26 +0500

    Begin14 added

---
+++ src/begins/Begin14.java
@@ -0,0 +1,16 @@
+package begins;
+
+import java.util.Scanner;
+
+public class Begin14 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Aylananing uzunligini kiriting: ");
+        double uzunlik = scanner.nextDouble();
+        double radius = uzunlik / (2 * Math.PI);
+        double yuzasi = radius * radius * Math.PI;
+        System.out.println("Aylananing radiusi: "+radius);
+        System.out.println("Aylananing yuzasi: "+yuzasi);
+    }
+}
