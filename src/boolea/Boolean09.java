9165c5cbd654fa72a3579d5d253ef9e0493f647c
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-15 22:43:44 +0500

    Boolean09 added

---
+++ src/boolea/Boolean09.java
@@ -0,0 +1,16 @@
+package boolea;
+
+import java.util.Scanner;
+
+public class Boolean09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Qiymat kiriting A: ");
+        int A = scanner.nextInt();
+        System.out.print("Qiymat kiriting B: ");
+        int B = scanner.nextInt();
+        int tekshir = (A % 2 != 0 || B % 2 != 0) ? 1 : 0;
+        System.out.println((tekshir == 1));
+
+    }
+}
