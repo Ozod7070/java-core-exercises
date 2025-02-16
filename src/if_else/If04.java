474b17b1eb8b284a45e5beca65fdb38265d45888
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-16 15:20:27 +0500

    If04 added

---
+++ src/if_else/If04.java
@@ -0,0 +1,24 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class If04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Son kiriting: ");
+        int son1 = scanner.nextInt();
+        System.out.println("Son kiriting: ");
+        int son2 = scanner.nextInt();
+        System.out.println("Son kiriting: ");
+        int son3 = scanner.nextInt();
+        if(son1 > 0 && son2 > 0 && son3 > 0){
+            System.out.println("3 ta musbat son mavjud");
+        }else if(son1 > 0 && son2 > 0 && son3 < 0 || son1 > 0 && son2 < 0 && son3 > 0 ||
+                son1 < 0 && son2 > 0 && son3 > 0 ){
+            System.out.println("2 ta musbat son mavjud");
+        }else{
+            System.out.println("1 ta musbat son mavjud");
+        }
+
+    }
+}
