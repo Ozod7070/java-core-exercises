43ffec9b7a6a61568c087142498fa30f9e072043
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-16 17:01:31 +0500

    If08 added

---
+++ src/if_else/If08.java
@@ -0,0 +1,22 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class If08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Son kiriting: ");
+        int son1 = scanner.nextInt();
+        System.out.print("Son kiriting: ");
+        int son2 = scanner.nextInt();
+        if(son1 > son2){
+            System.out.println(son1);
+            System.out.println(son2);
+        }else {
+            System.out.println(son2);
+            System.out.println(son1);
+        }
+    }
+}
+
+
