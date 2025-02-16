003b187cf241b5e78c6cfb3f7f64486c998a4b56
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-16 15:00:14 +0500

    Boolean40 added

---
+++ src/boolea/Boolean40.java
@@ -0,0 +1,24 @@
+package boolea;
+
+import java.util.Scanner;
+
+public class Boolean40 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Qiymat kiriting 1-8 gacha x1: ");
+        int x1 = scanner.nextInt();
+        System.out.print("Qiymat kiriting 1-8 gacha y1: ");
+        int y1 = scanner.nextInt();
+        System.out.print("Qiymat kiriting 1-8 gacha x2: ");
+        int x2 = scanner.nextInt();
+        System.out.print("Qiymat kiriting 1-8 gacha y2: ");
+        int y2 = scanner.nextInt();
+        if(Math.abs(x1 - x2) == 2 && Math.abs(y1 - y2) == 1 || Math.abs(x1 - x2) == 1 && Math.abs(y1 - y2) == 2 ){
+            System.out.println("Ot bir yurishda o'ta oladi");
+        }
+        else {
+            System.out.println("Ot bir yurishda o'ta olmaydi");
+        }
+
+    }
+}
