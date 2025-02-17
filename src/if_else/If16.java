d3a1c41243c09791bd0289f8ffa88a59606a239d
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-17 15:00:17 +0500

    If16 added

---
+++ src/if_else/If16.java
@@ -0,0 +1,24 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class If16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Son kiriting: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Son kiriting: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Son kiriting: ");
+        int num3 = scanner.nextInt();
+        if(num1 < num2 && num2 < num3){
+            System.out.println(num1 + num1);
+            System.out.println(num2 + num2);
+            System.out.println(num3 + num3);
+        }else{
+            System.out.println(-num1);
+            System.out.println(-num2);
+            System.out.println(-num3);
+        }
+    }
+}
