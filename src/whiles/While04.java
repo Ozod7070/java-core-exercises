0f75b1ddc6d2b0727d8012d0a4b8efd97014b643
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-18 15:45:15 +0500

    While04 added

---
+++ src/whiles/While04.java
@@ -0,0 +1,17 @@
+package whiles;
+
+import java.util.Scanner;
+
+public class While04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Qiynat kirting n: ");
+        int n = scanner.nextInt();
+        while (n == 3){
+        System.out.println("kiritgan soningiz 3 ning darajasi");
+            return;
+
+        }
+        System.out.println("kiritgan soningiz 3 ning darajasi emas ");
+    }
+}
