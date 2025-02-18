37f61034480a8520ae0ab78417b99a586041d733
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-18 19:22:14 +0500

    Array001 added

---
+++ src/arrays/Array001.java
@@ -0,0 +1,18 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Array001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Son kiriting: ");
+        int n = scanner.nextInt();
+        int [] arr = new  int[n];
+        for (int i = 0; i < n; i++) {
+          arr[i] = (2 * i) + 1;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
