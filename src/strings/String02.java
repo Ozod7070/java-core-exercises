d2c5f83226e48a50e950d422b44d99ebddcd37a5
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-24 20:09:45 +0500

    String02 added

---
+++ src/strings/String02.java
@@ -0,0 +1,13 @@
+package strings;
+
+import java.util.Scanner;
+
+public class String02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Son kiriting: ");
+        int n = scanner.nextInt();
+        char c = (char)(n);
+        System.out.println(c);
+    }
+}
