ccaafa5706b83008b3d8d4b338ed782fd77075af
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-25 17:24:48 +0500

    String17 added

---
+++ src/strings/String17.java
@@ -0,0 +1,13 @@
+package strings;
+
+import java.util.Scanner;
+
+public class String17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Satr kiriting: ");
+        String str = scanner.nextLine();
+        String res = str.toLowerCase();
+        System.out.println(res);
+    }
+}
