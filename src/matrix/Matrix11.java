35b0eae4d50516d7159a7c7e73c33cce5960d539
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-02-26 11:12:15 +0500

    Matrix11 added

---
+++ src/matrix/Matrix11.java
@@ -0,0 +1,39 @@
+package matrix;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Matrix11 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Etajlar uzunligini kiriting: ");
+        int n = scanner.nextInt();
+        System.out.print("Xonalar uzunligini kiriting: ");
+        int m = scanner.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = random.nextInt(11);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < m; j++) {
+                    System.out.print(arr[i][j] + " ");
+                }
+            } else {
+                for (int j = m - 1; j >= 0; j--) {
+                    System.out.print(arr[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
